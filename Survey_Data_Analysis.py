1aa05b5b3fdde901dbd2513f0cbd1aa957ca9946
ProUsability <thomas@lennig.org>
2015-12-09 00:45:47 -0800

    dictionaryIncrementor funct and spec counters
    - added function that checks a dictionary for a key and increments or
    initializes it with a value of 1
    - added several specification counters (browser version, operating
    system, plugins)

--- Survey_Data_Analysis.py
+++ Survey_Data_Analysis.py
@@ -9,12 +9,37 @@ import csv
 import datetime
 from user_agents import parse
 
+# function checks a dictionary for a value, increments the value or initializes it into the dict with a count of 1
+def dictionaryIncrementer(value, dictionary):
+    if value not in dictionary:
+        dictionary[value] = 1
+    else:
+        dictionary[value] += 1    
+
 count = 0
 notPC = 0
 bots = 0
 notPrimary = 0
 totalSecondsSpent = 0
 sampleSize = 0
+
+ffxBrowserVersion = {}
+crBrowserVersion = {}
+
+ffxPlugins = {}
+crPlugins = {}
+
+crOS = {}
+ffxOS = {}
+
+crOSVersion = {}
+ffxOSVersion = {}
+
+
+#crFlashVersion = {}
+#ffxFlashVersion = {}
+
+
 
 # Input file
 pfile
@@ -49,10 +74,13 @@ with open(pfile, 'rb') as csvfile:
 
             browserData = user_agent_ext.browser
             
-            if browserData.family != line[9]: # Skips if current browser is not primary broser
+            prefBrowser = line[9] # gets the preferred (primary) browser
+            
+            if browserData.family != prefBrowser: # Skips if current browser is not primary broser
                 notPrimary += 1
                 continue                
             
+            operatingSystem = user_agent_ext.os # gets the operating system information
             
             # calculate time spent per user
             started = datetime.datetime.strptime(line[2], '%m/%d/%Y %H:%M:%S') # converts start time to datetime object
@@ -61,7 +89,31 @@ with open(pfile, 'rb') as csvfile:
             secondsSpent = delta.seconds # converts time difference to seconds
             totalSecondsSpent += secondsSpent # tallies total seconds spent by all participants
             
-            # count versioning for both browsers
+            # gets browser versions            
+            version = browserData.version_string
+            osFamily = operatingSystem.family
+            osVersion = operatingSystem.version_string
+            
+            #flashversion = line[15]
+            
+            # count plugins for both browsers
+            plugins = line[17].split(',')
+            
+            if prefBrowser == 'Chrome':
+                dictionaryIncrementer(version,crBrowserVersion) # counts browser version
+                dictionaryIncrementer(osFamily,crOS) # counts operating system family
+                dictionaryIncrementer(osVersion,crOSVersion) # counts operating system versioning
+                for plugin in plugins:
+                    dictionaryIncrementer(plugin,crPlugins) # counts plugins
+            else: # if prefBrowser == 'Firefox':
+                dictionaryIncrementer(version,ffxBrowserVersion)
+                dictionaryIncrementer(osFamily,ffxOS)
+                dictionaryIncrementer(osVersion,ffxOSVersion)
+                for plugin in plugins:
+                    dictionaryIncrementer(plugin,ffxPlugins)
+                
+            
+            
             
             # Find the longest data value per column to make uploading to mysql easier
             
