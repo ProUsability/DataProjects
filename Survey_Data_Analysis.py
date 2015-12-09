bc655ed55d4375b63f65360b0d60ce80c4ff4827
ProUsability <thomas@lennig.org>
2015-12-08 23:16:54 -0800

    Core functionality added.
    - reads with csv library
    - processes and filters with user_agent library

--- Survey_Data_Analysis.py
+++ Survey_Data_Analysis.py
@@ -10,43 +10,72 @@ import csv
 # library for parsing out user_agent data
 from user_agents import parse
 
-# Input file
-fnamemaster = pfile
-fmaster = open(fnamemaster,'r')
-
-# Read in input file
-lines = fmaster.readlines()
-fmaster.close()
-
-#print lines
-
-
-length = len(lines)
-    
-# length of the document    
-print "This document has  " +  str(length) + "  lines"
-
-
-# Function for reading in the first two lines of data to determine the questions and answer options.
-
-# user_agent library for deciphering strings
-
-# Find the longest data value per column to make uploading to mysql easier
-
-# calculate time spent per user
-
 count = 0
+notPC = 0
+bots = 0
+notPrimary = 0
 
-for line in lines:
-    # print line
-    row = line.split(",")
-    count += 1
-    print row
-    print str(len(row))
+# Input file
+pfile
+with open(pfile, 'rb') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',', quotechar='"')
     
-    if count == 0 or count == 1:
-        continue
     
+    for line in lines:
+        print line
+        print len(line)
+        
+        print count
+        count += 1        
+        
+        # Function for reading in the first two lines of data to determine the questions and answer options.
+        if count == 1:
+            continue
+        elif count == 2:
+            continue
+        else:
+            # user_agent library for deciphering strings
+            # user_agent = parse(line[4]) # parses using data from "User Agent" field
+            user_agent_ext = parse(line[5]) # parses using data from "User Agent Extended" field
+            
+            if user_agent_ext.is_bot: # Skips if it's a bot
+                bots += 1
+                continue
+            
+            if user_agent_ext.is_pc is False: # Skips if not on desktop
+                notPC += 1
+                continue
+
+            browserData = user_agent_ext.browser
+            
+            if browserData.family != line[9]: # Skips if current browser is not primary broser
+                notPrimary += 1
+                continue                
+            
+            
+            # calculate time spent per user
+            
+            # count versioning for both browsers
+            
+            # Find the longest data value per column to make uploading to mysql easier
+            
+            
+            print browserData
+            
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
     
     
     
