719f79caebd2690abd4a485237b8e1d3da85fb97
ProUsability <thomas@lennig.org>
2015-12-14 01:38:16 -0800

    Testing SQL queries for uploaded data.

--- QueriesTestingMySQL.sql
+++ QueriesTestingMySQL.sql
@@ -1,13 +1,15 @@
 #CREATE DATABASE test;
 
-#USE test;
-
-#CREATE TABLE testtable;
+#CREATE TABLE testtable1;
 
 #DROP TABLE testtable1;
 
 #USE test;
 
-SELECT * FROM testtable1;
+#SELECT * FROM testtable1;
 
 #INSERT INTO testtable1 SET participant=45, primaryBrowser='Firefox', browserVersion='31',operatingSystem='Windows 7';
+
+#SELECT primaryBrowser, browserVersion, COUNT(*) from testtable1 GROUP BY primaryBrowser, browserVersion ORDER BY primaryBrowser, browserVersion;
+
+SELECT primaryBrowser, operatingSystem, COUNT(*) from testtable1 GROUP BY primaryBrowser, operatingSystem ORDER BY primaryBrowser, operatingSystem;
